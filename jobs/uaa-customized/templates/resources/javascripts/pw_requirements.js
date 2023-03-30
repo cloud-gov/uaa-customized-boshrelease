9e0bd2731e9de2f6e078b3a80efa2ba893bb9564
David Anderson <david.n.anderson@gsa.gov>
2023-03-30 13:28:30 -0400

    add default values

--- jobs/uaa-customized/templates/resources/javascripts/pw_requirements.js
+++ jobs/uaa-customized/templates/resources/javascripts/pw_requirements.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
   // Get all the password policy requirements.
   // In case the policy equals to null, initialize the values to 0.
   var lengthCount = <%= p("uaa.password.policy.minLength") %>;
-  var passwordField = document.getElementsByName("password")[0].value
-  var confirmPasswordField = document.getElementsByName("password_confirmation")[0].value;
-  var submitButton = document.getElementsByName("submit")[0].value;
+  var passwordField = document.getElementsByName("password")[0].value || "password";
+  var confirmPasswordField = document.getElementsByName("password_confirmation")[0].value || "password_confirmation";
+  var submitButton = document.getElementsByName("submit")[0].value || "submit";
   validatePassword(lengthCount,
                     passwordField,
                     confirmPasswordField,
